d848be51ea3f03160cc531b876a555360059962b
Naveen G <naveeng2402@gmail.com>
2022-05-29 08:19:56 +0530

    Updated ThunderClient

--- website/api/urls.py
+++ website/api/urls.py
@@ -7,6 +7,8 @@ router = routers.DefaultRouter()
 router.register(r"students", views.StudentModelViewSet)
 router.register(r"staffs", views.StaffModelViewSet)
 router.register(r"alumni-work", views.AlumniWorkModelViewSet)
+router.register(r"event-photos", views.EventPhotoModelViewSet)
+router.register(r"event-model", views.EventModelViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
@@ -14,4 +16,9 @@ urlpatterns = [
     path("alumni/", views.AlumniListView.as_view()),
     path("alumni/<int:id>", views.AlumniDetailView.as_view()),
     path("alumni-work/multivalued/<int:id>", views.AlumniWorkMultivaluedView.as_view()),
+    path("event/", views.EventListView.as_view()),
+    path("event/<int:id>", views.EventDetailView.as_view()),
+    path(
+        "event-photos/multivalued/<int:id>", views.EventPhotosMultivaluedView.as_view()
+    ),
 ]
