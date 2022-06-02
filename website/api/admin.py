61d90043e4e83a6a42311c83169b814451b23ddb
Naveen G <naveeng2402@gmail.com>
2022-06-02 06:08:59 +0530

    Added models to admin

--- website/api/admin.py
+++ website/api/admin.py
@@ -3,3 +3,17 @@ from . import models
 
 # Register your models here.
 admin.site.register(models.Student)
+admin.site.register(models.StudentAchievement)
+admin.site.register(models.Staff)
+admin.site.register(models.StaffAchievement)
+admin.site.register(models.Alumni)
+admin.site.register(models.AlumniWork)
+admin.site.register(models.Project)
+admin.site.register(models.Event)
+admin.site.register(models.EventPhoto)
+admin.site.register(models.EventVideo)
+admin.site.register(models.EventWinner)
+admin.site.register(models.AcademicCouncil)
+admin.site.register(models.AcademicCouncilMeeting)
+admin.site.register(models.Infrastructure)
+admin.site.register(models.InfrastructureImage)
